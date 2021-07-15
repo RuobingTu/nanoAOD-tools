432a8d9b3965c77ab17cbc4434db806f563d152c
Cristina <criss.ms7@gmail.com>
2021-07-15 17:42:46 -0500

    only select samples

--- condor/runSkim.py
+++ condor/runSkim.py
@@ -15,35 +15,35 @@ samples = {}
 samples["AK15"] = {
     "2017preUL_ak15": [
         "GravitonToHHToWWWW",
-        "GravitonToHHToWWWW_part1",
-        "GravitonToHHToWWWW_lnuqq",
-        "HHToBBVVToBBQQQQ_cHHH1",
-        "HHToBBVVToBBQQQQ_cHHH2p45",
-        "HHToBBVVToBBQQQQ_cHHH5",
-        "HHToBBVVToBBQQQQ_cHHH0",
-        "VBFHToWWToLNuQQ_M125_NNPDF31_TuneCP5_PSweights_13TeV_powheg_JHUGen710_pythia8",
-        "GluGluHToWWToLNuQQ_M125_NNPDF31_TuneCP5_PSweights_13TeV_powheg_JHUGen710_pythia8",
-        "GluGluZH_HToWW_M125_13TeV_powheg_pythia8_TuneCP5",
-        "HWminusJ_HToWW_M125_13TeV_powheg_pythia8_TuneCP5",
-        "HWplusJ_HToWW_M125_13TeV_powheg_pythia8_TuneCP5",
-        "HZJ_HToWW_M125_13TeV_powheg_jhugen714_pythia8_TuneCP5",
-        "GluGluToHHTo4V_node_cHHH1_TuneCP5_PSWeights_13TeV-powheg-pythia8",
-        "GluGluToHHTo2B2WToLNu2J_node_SM_TuneCP5_PSWeights_13TeV-madgraph-pythia8",
-        "BulkGravitonToHHTo4Q_MX-600to6000_MH-15to250_part2_TuneCP5_13TeV-madgraph_pythia8",
-        "BulkGravitonToHHTo4Q_MX-600to6000_MH-15to250_part1_TuneCP5_13TeV-madgraph_pythia8",
-        "QCD_HT300to500_TuneCP5_13TeV-madgraph-pythia8",
-        "QCD_HT500to700_TuneCP5_13TeV-madgraph-pythia8",
-        "QCD_HT700to1000_TuneCP5_13TeV-madgraph-pythia8",
-        "QCD_HT1000to1500_TuneCP5_13TeV-madgraph-pythia8",
+        #"GravitonToHHToWWWW_part1",
+        #"GravitonToHHToWWWW_lnuqq",
+        #"HHToBBVVToBBQQQQ_cHHH1",
+        ##"HHToBBVVToBBQQQQ_cHHH2p45",
+        #"HHToBBVVToBBQQQQ_cHHH5",
+        #"HHToBBVVToBBQQQQ_cHHH0",
+        #"VBFHToWWToLNuQQ_M125_NNPDF31_TuneCP5_PSweights_13TeV_powheg_JHUGen710_pythia8",
+        #"GluGluHToWWToLNuQQ_M125_NNPDF31_TuneCP5_PSweights_13TeV_powheg_JHUGen710_pythia8",
+        #"GluGluZH_HToWW_M125_13TeV_powheg_pythia8_TuneCP5",
+        #"HWminusJ_HToWW_M125_13TeV_powheg_pythia8_TuneCP5",
+        #"HWplusJ_HToWW_M125_13TeV_powheg_pythia8_TuneCP5",
+        #"HZJ_HToWW_M125_13TeV_powheg_jhugen714_pythia8_TuneCP5",
+        #"GluGluToHHTo4V_node_cHHH1_TuneCP5_PSWeights_13TeV-powheg-pythia8",
+        #"GluGluToHHTo2B2WToLNu2J_node_SM_TuneCP5_PSWeights_13TeV-madgraph-pythia8",
+        #"BulkGravitonToHHTo4Q_MX-600to6000_MH-15to250_part2_TuneCP5_13TeV-madgraph_pythia8",
+        #"BulkGravitonToHHTo4Q_MX-600to6000_MH-15to250_part1_TuneCP5_13TeV-madgraph_pythia8",
+        # "QCD_HT300to500_TuneCP5_13TeV-madgraph-pythia8",
+        # "QCD_HT500to700_TuneCP5_13TeV-madgraph-pythia8",
+        # "QCD_HT700to1000_TuneCP5_13TeV-madgraph-pythia8",
+        # "QCD_HT1000to1500_TuneCP5_13TeV-madgraph-pythia8",
         "QCD_HT1500to2000_TuneCP5_13TeV-madgraph-pythia8",
-        "QCD_HT2000toInf_TuneCP5_13TeV-madgraph-pythia8",
-        "TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8",
-        "TTToHadronic_TuneCP5_13TeV-powheg-pythia8",
-        "WJetsToLNu_HT-1200To2500_TuneCP5_13TeV-madgraphMLM-pythia8",
-        "WJetsToLNu_HT-400To600_TuneCP5_13TeV-madgraphMLM-pythia8",
-        "WJetsToLNu_HT-800To1200_TuneCP5_13TeV-madgraphMLM-pythia8",
-        "WJetsToLNu_HT-2500ToInf_TuneCP5_13TeV-madgraphMLM-pythia8",
-        "WJetsToLNu_HT-600To800_TuneCP5_13TeV-madgraphMLM-pythia8",
+        # "QCD_HT2000toInf_TuneCP5_13TeV-madgraph-pythia8",
+        # "TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8",
+        # "TTToHadronic_TuneCP5_13TeV-powheg-pythia8",
+        # "WJetsToLNu_HT-1200To2500_TuneCP5_13TeV-madgraphMLM-pythia8",
+        # "WJetsToLNu_HT-400To600_TuneCP5_13TeV-madgraphMLM-pythia8",
+        # "WJetsToLNu_HT-800To1200_TuneCP5_13TeV-madgraphMLM-pythia8",
+        # "WJetsToLNu_HT-2500ToInf_TuneCP5_13TeV-madgraphMLM-pythia8",
+        # "WJetsToLNu_HT-600To800_TuneCP5_13TeV-madgraphMLM-pythia8",
     ],
 }
 
@@ -57,9 +57,9 @@ def submit_job(dataset, files, tag, jet, test=False):
     os.system("eos root://cmseos.fnal.gov/ mkdir -p %s"%outdir) 
             
     # max-entries (for input only now)
-    nentries = 8000
-    if "HH" or "HToWW" in dataset:
-        nentries = 100000
+    #nentries = 4000
+    #if "HH" or "HToWW" in dataset:
+    nentries = 10000
 
     # script to run
     execname = "run_skim_input.sh"
